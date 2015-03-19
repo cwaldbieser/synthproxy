c213cfb5bb769c66564d201ce1a241c6f8152b6c
Carl Waldbieser <cwaldbieser@gmail.com>
2015-03-18 22:46:54 -0400

    Added support for proxying multiple LDAP servers using a round-robin scheme.
    Each node in the cluster conects to each proxy in a cycle to fulfill requests.

--- proxies/synthproxy.py
+++ proxies/synthproxy.py
@@ -9,6 +9,7 @@ import json
 import os.path
 import sys
 from urlparse import urlparse
+from configutils import MultiLocationLDAPConfig
 import httpclient
 from proxies.lru import LRUTimedCache, LRUClusterProtocolFactory, \
                         LRUClusterClient, make_cluster_func
@@ -229,7 +230,7 @@ def validate_config(config):
     """
     required = {
         'CouchDB': frozenset(['url', 'user', 'passwd']),
-        'LDAP': frozenset(['proxied_url',]),
+        'LDAP': []
         } 
     def isValidClusterOption(opt):
         if opt == "endpoint":
@@ -237,10 +238,16 @@ def validate_config(config):
         if opt.startswith("peer"):
             return True
         return False
+    def isValidLDAPOption(opt):
+        if opt in ['proxy_cert', 'use_starttls']:
+            return True
+        if opt.startswith("proxied_url"):
+            return True
+        return False
     optional = {
         'Application': lambda x: x in frozenset([
             'debug', 'debug_cache', 'endpoint', 'search_cache_lifetime', 'search_cache_size']),
-        'LDAP': lambda x: x in frozenset(['proxy_cert', 'use_starttls']),
+        'LDAP': isValidLDAPOption,
         'Cluster': isValidClusterOption
         }
     valid = True
@@ -304,8 +311,20 @@ class SynthProxyService(service.Service):
         db_url = scp.get("CouchDB", "url")
         db_user = scp.get("CouchDB", "user")
         db_passwd = scp.get("CouchDB", "passwd") 
-        proxied_scheme, proxied_host, proxied_port = parse_url(
-            scp.get("LDAP", "proxied_url"))
+        proxied_locations = []
+        last_proxied_scheme = None
+        for opt in scp.options("LDAP"):
+            if opt.startswith("proxied_url"):
+                proxied_scheme, proxied_host, proxied_port = parse_url(
+                    scp.get("LDAP", opt))
+                if last_proxied_scheme is None:
+                    last_proxied_scheme = proxied_scheme
+                elif proxied_scheme != last_proxied_scheme:
+                    log.msg(
+                        "[ERROR] Mixed LDAP schemes ('{0}' and '{1}') are not implemented.".format(
+                            last_proxied_scheme, proxied_scheme))
+                    sys.exit(1)
+                proxied_locations.append((proxied_host, proxied_port))
         factory = protocol.ServerFactory()
         if scp.has_option("LDAP", "proxy_cert"):
             proxy_cert = scp.get("LDAP", "proxy_cert")
@@ -313,12 +332,19 @@ class SynthProxyService(service.Service):
                 certData = f.read()
             cert = ssl.PrivateCertificate.loadPEM(certData)
             factory.options = cert.options()
-        proxied = (proxied_host, proxied_port)
         if proxied_scheme == 'ldaps':
             log.msg("[ERROR] `ldaps` scheme is not supported.")
             sys.exit(0)
         use_tls = scp.getboolean('LDAP', 'use_starttls')
-        cfg = config.LDAPConfig(serviceLocationOverrides={'': proxied, })
+        if len(proxied_locations) == 0:
+            log.msg("[ERROR] No proxied locations specified.")
+        elif len(proxied_locations) == 1:
+            proxied = proxied_locations[0]
+            slo = {'': proxied, }
+            cfg = config.LDAPConfig(serviceLocationOverrides=slo)
+        else:
+            slo = {'': proxied_locations}
+            cfg = MultiLocationLDAPConfig(serviceLocationOverrides=slo)
         if scp.has_option('Application', 'debug'):
             debug_app = scp.getboolean('Application', 'debug')
         else:
